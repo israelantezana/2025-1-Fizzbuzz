2c3f79e3bbefa25b622b0a27f6ff367d06ca2a75
Israel Antezana Rojas <israelantezana@gmail.com>
2025-02-20 21:52:25 -0400

    feat: genera el Fizz con el numero 3

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -8,4 +8,7 @@ describe("Fizzzbuzz", () => {
   it("deberia generar el mismo numero si el numero no sigue una regla", () => {
     expect(fizzbuzz(2)).toEqual("2");
   });
+  it("deberia generar fizz si el numero ingresado es 3", () => {
+    expect(fizzbuzz(3)).toEqual("Fizz");
+  });
 });
